e99343b43a3cc01ec40ce922aa7479f187e730f5
Victor Caruso <vic.caruso@gmail.com>
2022-02-18 14:00:44 -0800

    Created Navigation component w/ Logout

--- functions/tmdb.js
+++ functions/tmdb.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 
 exports.handler = async (event) => {
   try {
-    const movieId = 500;
+    const movieId = 666;
     const response = await fetch(`https://api.themoviedb.org/3/movie/${movieId}?api_key=${process.env.TMDB_KEY}&language=en-US`);
     const data = await response.json();
     const json = JSON.stringify({ data });
--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import { useEffect, useState } from 'react';
 import { BrowserRouter, Route, Switch, Link, NavLink, Redirect } from 'react-router-dom';
 import './App.css';
 import AuthPage from './AuthPage';
+import Navigation from './Navigation';
 import { getUser } from './services/supabase-utils';
 
 function App() {
-  const [userSession, setUserSession] = useState(null);
+  const [userSession, setUserSession] = useState(getUser());
 
   useEffect(() => {
     setUserSession(getUser());
@@ -14,6 +15,9 @@ function App() {
   return (
     <BrowserRouter>
       <div>
+        {
+          userSession && <Navigation />
+        }
         <Switch>
           <Route exact path='/'>
             {
@@ -39,7 +43,7 @@ function App() {
           <Route exact path='/auth'>
             {
               !userSession //user does not have active session
-                ? <AuthPage setUserSession={ setUserSession } />
+                ? <AuthPage setUserSession={setUserSession} />
                 : <Redirect to='/' />
             }
           </Route>
