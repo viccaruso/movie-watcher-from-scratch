0cae3250f0fabac63abea48ca149bec2c9d7723c
Victor Caruso <vic.caruso@gmail.com>
2022-02-17 17:51:13 -0800

    set up routes for conditional routing based on whether user is logged in or not

--- src/App.js
+++ src/App.js
@@ -1,10 +1,45 @@
+import { useState } from 'react';
+import { BrowserRouter, Route, Switch, Link, NavLink, Redirect } from 'react-router-dom';
 import './App.css';
 
 function App() {
+  const [userSession, setUserSession] = useState('');
   return (
-    <div className="App">
-      
-    </div>
+    <BrowserRouter>
+      <div>
+        <Switch>
+          <Route exact path='/'>
+            {
+              userSession
+                ? <Redirect to='/list' />
+                : <Redirect to='/auth' />
+            }
+          </Route>
+          <Route exact path='/list'>
+            {
+              userSession
+                ? <p>/list</p>
+                : <Redirect to='auth' />
+            }
+          </Route>
+          <Route exact path='/search'>
+            {
+              userSession
+                ? <p>/search</p>
+                : <Redirect to='/auth' />
+            }
+          </Route>
+          <Route exact path='/auth'>
+            {
+              !userSession //user does not have active session
+                ? <p>/auth</p>
+                : <Redirect to='/' />
+            }
+          </Route>
+
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
